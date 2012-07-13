649907c77283cacbce324b1b2eabb6f1dc62aeb9
Kritivasas <kritivasas.shukla@studypadinc.com>
2012-07-13 20:16:30 +0530

    a little improved ui

--- app/views/layouts/presentation.rb
+++ app/views/layouts/presentation.rb
@@ -2,7 +2,9 @@
 <html>
 <head>
   <title>AppForPresentation</title>
+  <%= stylesheet_link_tag    "application_temp" %>
   <%= javascript_include_tag "application" %>
+  <%#= javascript_include_tag "application-884572fca56bfc7fa144afebd01a49b8" %> 
   <%= csrf_meta_tags %>
 </head>
 <body>
